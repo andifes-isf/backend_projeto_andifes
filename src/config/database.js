06e7792ff1e5fcd754876aa0cd19e33511e95580
Rafael Naoki Arakaki Uyeta <86424714+Arata2703@users.noreply.github.com>
2024-07-30 12:31:47 -0300

    add: .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+import "dotenv/config"
+
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'programaisf_development',
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     define:{
         timestamps: true,
     }
