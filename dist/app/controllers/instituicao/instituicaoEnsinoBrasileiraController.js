4f1022c9ed2c9e004237877967143972880b89ae
Rafael Naoki Arakaki Uyeta <86424714+Arata2703@users.noreply.github.com>
2024-10-28 16:13:57 -0300

    utilizando arquivo de string na instituição
    uso do arquivo de string nos seguintes controllers:
    - instituicaoEnsinoController
    - instituicaoEnsinoBrasileiraController
    - instituicaoEnsinoEstrangeiraController

--- dist/app/controllers/instituicao/instituicaoEnsinoBrasileiraController.js
+++ dist/app/controllers/instituicao/instituicaoEnsinoBrasileiraController.js
@@ -1,43 +1,44 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { newObj[key] = obj[key]; } } } newObj.default = obj; return newObj; } } function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _yup = require('yup'); var Yup = _interopRequireWildcard(_yup);
 var _instituicaoensinobrasileira = require('../../models/instituicao/instituicaoensinobrasileira'); var _instituicaoensinobrasileira2 = _interopRequireDefault(_instituicaoensinobrasileira);
 var _instituicaoEnsinoController = require('./instituicaoEnsinoController'); var _instituicaoEnsinoController2 = _interopRequireDefault(_instituicaoEnsinoController);
+var _messages_pt = require('../../utils/messages/messages_pt'); var _messages_pt2 = _interopRequireDefault(_messages_pt);
 
 class instituicaoEnsinoBrasileiraController {
     async post(req, res){
         try {
             await _instituicaoEnsinoController2.default.post(req, res, 1)
 
-            const instituicaoExistente = await _instituicaoensinobrasileira2.default.findOne({
+            const existingInstitution = await _instituicaoensinobrasileira2.default.findOne({
                 where: {
                     CNPJ: req.body.CNPJ
                 }
             })
     
-            if(instituicaoExistente) {
+            if(existingInstitution) {
                 return res.status(409).json({
-                    msg: "Instituicao de Ensino Brasileira ja cadastrada"
+                    error: `${existingInstitution.sigla} ` + _messages_pt2.default.ALREADY_IN_SYSTEM
                 })
             }
     
-            const instituicao = await _instituicaoensinobrasileira2.default.create({
+            const institution = await _instituicaoensinobrasileira2.default.create({
                 CNPJ: req.body.CNPJ,
                 idInstituicao: req.body.idInstituicao,
                 sigla: req.body.sigla
             })
     
-            return res.status(201).json(instituicao)
+            return res.status(201).json(institution)
         } catch (error) {
-            return res.status(500).json("Ocorreu um erro interno no servidor: " + error)
+            return res.status(500).json(_messages_pt2.default.INTERNAL_SERVER_ERROR + error)
         }
     }
 
     async get(_, res) {
         try {
-            const instituicoes = await _instituicaoensinobrasileira2.default.findAll()
+            const institutions = await _instituicaoensinobrasileira2.default.findAll()
 
-            return res.status(200).json(instituicoes)
+            return res.status(200).json(institutions)
         } catch (error) {
-            return res.status(500).json("Ocorreu um erro interno no servidor: " + error)
+            return res.status(500).json(_messages_pt2.default.INTERNAL_SERVER_ERROR + error)
         }
     }
 }
