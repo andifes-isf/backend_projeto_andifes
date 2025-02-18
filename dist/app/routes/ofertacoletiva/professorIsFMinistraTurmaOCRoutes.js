493ae608aab03fc7be599348577dc04e92a47e18
Rafael Naoki Arakaki Uyeta <86424714+Arata2703@users.noreply.github.com>
2025-02-18 15:20:49 -0300

    Removendo diretório dist do rastreamento e adicionando ele ao .gitignore

--- dist/app/routes/ofertacoletiva/professorIsFMinistraTurmaOCRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _professorIsFMinistraTurmaOCController = require('../../controllers/ofertacoletiva/professorIsFMinistraTurmaOCController'); var _professorIsFMinistraTurmaOCController2 = _interopRequireDefault(_professorIsFMinistraTurmaOCController);
-var _auth = require('../../middlewares/auth'); var _auth2 = _interopRequireDefault(_auth);
-
-const router = new (0, _express.Router)()
-
-router.post('/:idTurma', _auth2.default, _professorIsFMinistraTurmaOCController2.default.post)
-
-exports. default = router
