5d5c324f5fb858cc2f8cbcf4ccc49a909ae8ede9
Rafael Naoki Arakaki Uyeta <86424714+Arata2703@users.noreply.github.com>
2025-02-18 14:19:24 -0300

    Removendo diretório dist do rastreamento e adicionando ele ao .gitignore

--- dist/app/routes/instituicao/instituicaoEnsinoRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _instituicaoEnsinoController = require('../../controllers/instituicao/instituicaoEnsinoController'); var _instituicaoEnsinoController2 = _interopRequireDefault(_instituicaoEnsinoController);
-
-const router = new (0, _express.Router)()
-
-router.get('/', _instituicaoEnsinoController2.default.get)
-
-exports. default = router
