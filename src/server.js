490be003bf9c40a5b93a6c1a063150c80dacffd8
Lucas Balieiro <97367138+LucasMBalieiro@users.noreply.github.com>
2025-03-07 17:19:25 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from "./app"
 import './database'
 
-app.listen(8800)
+// Listen on all network interfaces
+app.listen(8800, "0.0.0.0", () => {
+  console.log("Server is running on port 8800");
+});
